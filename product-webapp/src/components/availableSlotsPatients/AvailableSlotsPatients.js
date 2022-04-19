b44fc3cde56bd23a9ce12a6c8066e3be82b00d57
Jatin_Chugh <jatin.chugh@globallogic.com>
2022-04-19 16:43:56 +0530

    box shadow in buttons and some layout and styling chamges in appointmentslotsPatients component

--- product-webapp/src/components/availableSlotsPatients/AvailableSlotsPatients.js
+++ product-webapp/src/components/availableSlotsPatients/AvailableSlotsPatients.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+
 import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { Calendar } from "react-calendar";
@@ -6,20 +6,24 @@ import 'react-calendar/dist/Calendar.css';
 import '../../component.css';
 import AvailableSlotschips from "./AvailableSlotChips.js";
 import AppointmentService from "../../services/appointment.service";
+import moment from "moment";
 
 function AvailableSlotsPatients() {
 
     const [result, setresult] = useState([]);
-    const [value, onChange] = useState(new Date());
+    const [value] = useState(new Date());
     const [date, setDate] = useState('');
-    const [noSlots, setNoSlots] = useState(false);
     const [details, setDetails] = useState({});
-    const [bookedAppointments, setbookedAppointments] = useState([]);
+    // const [bookedAppointments, setbookedAppointments] = useState([]);
 
     function changeDate(value, event) {
-        let a = value.toString();
-        let date = a.substring(4, 15);
-        setDate(date);
+        let momentDate = moment(value).format('DD/MM/YYYY');
+        console.log(momentDate);
+        setDate(momentDate);
+
+        // let a = value.toString();
+        // let date = a.substring(4, 15);
+        // setDate(date);
     }
 
     let appointmentService = new AppointmentService();
@@ -34,8 +38,8 @@ function AvailableSlotsPatients() {
 
     const bookAppointment = () =>{
         appointmentService.getBookedAppointment().then((response) =>{
-            let data = response.data;
-            setbookedAppointments = data;
+            // let data = response.data;
+            // setbookedAppointments = data;
 
         })
     }
@@ -43,14 +47,14 @@ function AvailableSlotsPatients() {
     return (
         <div className="container-fluid">
             <div className="row calender-doctor-details">
-                <div className="col-8 calender-container">
+                <div className="col-md-8 col-sm-12 calender-container">
 
                     <Calendar onChange={changeDate} value={value} />
                 </div>
-                <div className="col-4">
+                <div className="col-md-4 col-sm-12">
                     <div className="doctors-details column">
                         <div className="col mb-1 mt-4">
-                            <img src="../Doctor_image.jpg"  className="doctor-image" />
+                            <img src="../Doctor_image.jpg"  className="doctor-image" alt="" />
                         </div>
                         <div className="col mb-4">
                             <h6>Dr. Jatin Chugh</h6>
@@ -78,8 +82,13 @@ function AvailableSlotsPatients() {
                                     slotEndTime={response.slotEndTime}
                                 />
                             )
+                        }else{
+                            return(
+                               console.log('No slots found')
+                            )
                         }
                     })
+                    
                 }
             </div>
             <div className="book-appointment">
