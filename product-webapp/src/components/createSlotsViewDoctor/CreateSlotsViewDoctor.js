09dc04b8949d040cd7d5d0207da423eb015cc0c6
Jatin_Chugh <jatin.chugh@globallogic.com>
2022-05-04 17:43:51 +0530

    calendar changes

--- product-webapp/src/components/createSlotsViewDoctor/CreateSlotsViewDoctor.js
+++ product-webapp/src/components/createSlotsViewDoctor/CreateSlotsViewDoctor.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import moment from "moment";
-import '../../component.css';
 import { Calendar } from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
 import AppointmentService from "../../services/appointment.service";
