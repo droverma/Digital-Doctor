51fefe7cf10369ceacd75752be609a60bf45f8a1
drishti.verma <drishti.1@globallogic.com>
2022-05-10 13:06:49 +0530

    chnages in index

--- product-webapp/product-webapp/src/index.js
+++ product-webapp/product-webapp/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ContextProvider } from './context/Context';
 import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
