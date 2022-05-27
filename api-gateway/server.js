4fd0733600eff3f033abd8d1cee2c58b0be5fb3f
Jatin_Chugh <jatin.chugh@globallogic.com>
2022-05-27 15:50:27 +0530

    changes for docker config files

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,33 +1,33 @@
 const gateway = require("fast-gateway");
-
-// const cors = require("cors");
-// app.use(cors());
+const express = require("express");
+const cors = require("cors")
+const app = express();
 const PORT = 8080;
 
 const server = gateway({
   routes: [
     {
-      prefix: "/user-service",
+      prefix: "user-service",
       target: "http://localhost:8085",
     },
     {
-      prefix: "/slot-service",
+      prefix: "slot-service",
       target: "http://localhost:8084",
     },
     {
-      prefix: "/appointment-service",
+      prefix: "appointment-service",
       target: "http://localhost:8081",
     },
     {
-      prefix: "/video-chat-service",
+      prefix: "video-chat-service",
       target: "http://localhost:8083",
     },
     {
-      prefix: "/email-service",
+      prefix: "email-service",
       target: "http://localhost:8082",
     },
     {
-      prefix: "/socket-service",
+      prefix: "socket-service",
       target: "http://localhost:5000",
     },
     {
@@ -37,10 +37,16 @@ const server = gateway({
   ],
 });
 
+const corsOptions = {
+  origin: ["http://localhost:3003", "http://localhost:3000"],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 server.get("/test", (req, res) => {
   res.json({ message: "Server Called" });
 });
 
 server.start(PORT).then((server) => {
-  console.log(`Gateway is running on port http://127.0.0.1:${PORT}`);
+  console.log(`Gateway is running on port http://localhost:${PORT}`);
 });
